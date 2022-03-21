b8279bbbfcb3d5e835ad57056987ff3104e188f7
Ribbon Five <anton.franzen@icloud.com>
2022-03-22 00:57:48 +0100

    test seearchbar

--- src/components/AppShell/Nav/Searchbar/Searchbar/SearchBarComponent.test.js
+++ src/components/AppShell/Nav/Searchbar/Searchbar/SearchBarComponent.test.js
@@ -17,3 +17,35 @@ test("renders learn react link", () => {
   expect(screen.getByTestId("searchbarInputID")).toHaveFocus();
   //    fireEvent.click(screen.getByText("Load Greeting"));
 });
+
+test("Input is valid, expect menu should open", () => {
+  const onSearchHandler = jest.fn();
+  render(
+    <SearchbarComponent
+      onResultClickHandler={() => console.log("1")}
+      onSearchHandler={onSearchHandler}
+      //   onSearchHandler={(input) => {
+      //     if (input === "bitcoin") {
+      //       return [1, 2, 3];
+      //     } else {
+      //       return null;
+      //     }
+      //   }}
+    />
+  );
+
+  expect(onSearchHandler.mock.calls).toBeGreaterThan(0);
+
+  // Get the input search bar
+  // Add bitcoin as input
+  // Expect a list
+
+  //   expect(screen.getAllByTestId("searchbarID")).toHaveLength(1);
+  //   screen.getAllByTestId("searchbarID");
+
+  //   fireEvent.click(screen.getByTestId("searchbarID"));
+  //   expect(screen.getByTestId("searchbarInputID")).toHaveFocus();
+  //    fireEvent.click(screen.getByText("Load Greeting"));
+});
+
+// Input is valid, expect menu should open
